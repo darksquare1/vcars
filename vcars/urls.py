f411b99fa7a4fcb7b4d25053bf58e17d1b0af48b
nekit <nekit05@list.ru>
2024-08-25 19:13:25 +0900

    added footer, search by clicking on tags

--- vcars/urls.py
+++ vcars/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'vcars'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('tag/<slug:tag_slug>/', views.index, name='tagged_index')
 ]
