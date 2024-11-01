95b5211d94a88c55d10e8b0b3f9a205becbfe3f5
depressedkid <nekit05@list.ru>
2024-11-01 21:54:22 +1000

    added model Message

--- chat/models.py
+++ chat/models.py
@@ -8,15 +8,31 @@ class ChatGroup(models.Model):
     uuid = models.UUIDField(default=uuid4, editable=False)
     name = models.CharField(max_length=25)
     members = models.ManyToManyField(User)
+
     def __str__(self):
         return f'Group {self.name}-{self.uuid}'
+
     def get_absolute_url(self):
         return reverse('chat:group', args=[str(self.uuid)])
+
     def add_user_to_group(self, user):
         self.members.add(user)
         self.event.create(type='Join', user=user)
         self.save()
+
     def remove_user_from_group(self, user):
         self.members.remove(user)
         self.event.create(type='Left', user=user)
         self.save()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        date = self.timestamp.date()
+        time = self.timestamp.time()
+        return f"{self.author}:- {self.body} @{date} {time.hour}:{time.minute}"
