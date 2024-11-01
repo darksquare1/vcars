ecd5525bef54c71cc2d0c4004b8195f13ec18050
depressedkid <nekit05@list.ru>
2024-11-01 21:03:25 +1000

    added model ChatGroup

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from uuid import uuid4
 
-# Create your models here.
+
+class ChatGroup(models.Model):
+    uuid = models.UUIDField(default=uuid4, editable=False)
+    name = models.CharField(max_length=25)
+    members = models.ManyToManyField(User)
+    def __str__(self):
+        return f'Group {self.name}-{self.uuid}'
+    def get_absolute_url(self):
+        return reverse('chat:group', args=[str(self.uuid)])
+    def add_user_to_group(self, user):
+        self.members.add(user)
+        self.event.create(type='Join', user=user)
+        self.save()
+    def remove_user_from_group(self, user):
+        self.members.remove(user)
+        self.event.create(type='Left', user=user)
+        self.save()
