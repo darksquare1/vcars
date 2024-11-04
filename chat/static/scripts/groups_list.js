2a7228253179377c07c11d453e6510961f1b9515
depressedkid <nekit05@list.ru>
2024-11-04 19:39:04 +1000

    updated groups_list.js script

--- chat/static/scripts/groups_list.js
+++ chat/static/scripts/groups_list.js
@@ -2,11 +2,22 @@ const base_url = `${window.location.hostname}:${window.location.port}${window.lo
 const websocket = new WebSocket(`ws://${base_url}`)
 
 websocket.onopen = function (event) {
-    console.log('client connected')
-    websocket.send('client says hi')
+    console.log('connection opened')
 }
 websocket.onmessage = function (event) {
-    console.log('client event', event)
+    let message = JSON.parse(event.data)
+    let type = message.type
+    let data = message.data
+    switch (type) {
+        case "leave_group":
+            leave_group_handler(data)
+            break
+
+        case "join_group":
+            join_group_handler(data)
+            break
+    }
+
 }
 
 function add_event_to_buttons() {
@@ -29,8 +40,33 @@ function send_event_message(event) {
             "type": action,
             "data": group_uuid,
         }
+
         websocket.send(JSON.stringify(data))
     }
+}
+
+function leave_group_handler(uuid) {
+    let leave_button = document.getElementById(`leave-${uuid}`)
+    let open_button = document.getElementById(`open-${uuid}`)
+    leave_button.remove()
+    open_button.remove()
+    let join_button = `<button id="join-${uuid}" class="group-option btn btn-success btn-sm" value="join_group ${uuid}">Join</button>`
+    let dev_body = document.getElementById(uuid)
+
+    dev_body.innerHTML += join_button
+    add_event_to_buttons()
+}
+
+
+function join_group_handler(uuid) {
+    let join_button = document.getElementById(`join-${uuid}`)
+    join_button.remove()
+    let leave_button = `<button id="leave-${uuid}" class="group-option btn btn-danger btn-sm me-2" value="leave_group ${uuid}">Leave</button>`
+    let open_button = `<button id="open-${uuid}" class="group-option btn btn-primary btn-sm" value="open_group ${uuid}">Open</button>`
+    let dev_body = document.getElementById(uuid)
+    dev_body.innerHTML += leave_button
+    dev_body.innerHTML += open_button
+    add_event_to_buttons()
 }
 
 add_event_to_buttons()
