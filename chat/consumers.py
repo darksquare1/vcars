caa5664de26c7057f2c225c024d6e12a4241ee0a
depressedkid <nekit05@list.ru>
2024-11-06 00:25:39 +1000

    added chat.signals

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,6 +1,5 @@
 import json
 
-from asgiref.sync import sync_to_async
 from django.contrib.auth.models import User
 from channels.db import database_sync_to_async
 from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
