04f4fedffcfc0f0a4a2185d8c0a37639dec0fa73
depressedkid <nekit05@list.ru>
2024-11-15 21:19:38 +1000

    added play button, optimized spriteAnimation

--- vcars/static/scripts/game.js
+++ vcars/static/scripts/game.js
@@ -6,27 +6,29 @@
 
     const car = document.querySelector('.car')
     const bushes = document.querySelectorAll('.bush')
-
+    const mountains = document.querySelectorAll('.mountain')
     const bush1 = bushes[0]
+    const coordsBush1 = getCoords(bush1)
 
     animationId = requestAnimationFrame(startGame)
 
     function startGame() {
-        treesAnimation();
-
+        spritesAnimation()
         animationId = requestAnimationFrame(startGame)
     }
 
-    function treesAnimation() {
-        const newCoord = getYCoord(bush1) + speed
-        bush1.style.transform = `translateY(${newCoord}px)`
+    function spritesAnimation() {
+        const newY = coordsBush1.y + speed
+        coordsBush1.y = newY
+        bush1.style.transform = `translate(${coordsBush1.x}px, ${newY}px)`
     }
 
-    function getYCoord(element) {
+    function getCoords(element) {
         const matrix = window.getComputedStyle(element).transform
         const array = matrix.split(',')
-        const lastElement = array[array.length - 1]
-        return parseFloat(lastElement)
+        const y = array[array.length - 1]
+        const x = array[array.length - 2]
+        return {x: parseFloat(x), y: parseFloat(y)}
     }
 
     const gameButton = document.querySelector('.game-button')
@@ -37,8 +39,9 @@
             gameButton.children[0].style.display = 'none'
             gameButton.children[1].style.display = 'initial'
         } else {
+            aminationId = requestAnimationFrame(startGame)
             gameButton.children[0].style.display = 'initial'
             gameButton.children[1].style.display = 'none'
         }
-    });
-})();
+    })
+})()
