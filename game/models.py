3529396757681cde97f99024d62f674b88ed9f75
depressedkid <nekit05@list.ru>
2024-11-18 19:09:46 +1000

    added Record model

--- game/models.py
+++ game/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
 
+
+class Record(models.Model):
+    record = models.PositiveIntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.record
